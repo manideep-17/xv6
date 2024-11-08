096c67b7d87c524d7252bf03c0ad15e4c8dd8bc6
Manideep Reddy Aliminati <manideepreddyaliminati@Manideeps-Laptop.local>
2024-11-08 10:29:33 -0700

    Init code

--- xv6-riscv/user/ulthread.c
+++ xv6-riscv/user/ulthread.c
@@ -7,39 +7,157 @@
 
 /* Standard definitions */
 #include <stdbool.h>
-#include <stddef.h> 
+#include <stddef.h>
+
+static ulthread ulthred_table[MAXULTHREADS];
 
 /* Get thread ID */
-int get_current_tid(void) {
+int get_current_tid(void)
+{
     return 0;
 }
 
+struct ulthread *scheduler_thread = NULL;
+static ulthread *current_thread = NULL;
+
+static ulthread_scheduling_algorithm scheduling_algorithm = ROUNDROBIN;
+static uint64 global_thread_counter = 0;
 /* Thread initialization */
-void ulthread_init(int schedalgo) {}
+void ulthread_init(int schedalgo)
+{
+    memset(ulthred_table, 0, sizeof(ulthred_table));
+    for (int i = 0; i < MAXULTHREADS; i++)
+    {
+        ulthred_table[i].state = FREE;
+    }
+    scheduling_algorithm = schedalgo;
+
+    // Create the scheduler thread using the current execution context
+    current_thread = &ulthred_table[0];
+    current_thread->ultid = 0;
+    current_thread->state = RUNNABLE;
+    current_thread->priority = 0;
+    current_thread->creation_time = global_thread_counter++;
+
+    // Set the current thread to the scheduler thread
+    current_thread = scheduler_thread;
+}
 
 /* Thread creation */
-bool ulthread_create(uint64 start, uint64 stack, uint64 args[], int priority) {
-    /* Please add thread-id instead of '0' here. */
-    printf("[*] ultcreate(tid: %d, ra: %p, sp: %p)\n", 0, start, stack);
+bool ulthread_create(uint64 start, uint64 stack, uint64 args[], int priority)
+{
+    ulthread *ult = NULL;
+    for (int i = 0; i < MAXULTHREADS; i++)
+    {
+        if (ulthred_table[i].ultid == 0)
+        {
+            ult = &ulthred_table[i];
+            ult->ultid = i;
+            break;
+        }
+    }
 
-    return false;
+    if (ult == NULL)
+    {
+        printf("No available thread slots\n");
+        return false;
+    }
+
+    ult->state = RUNNABLE;
+    ult->priority = priority;
+    ult->start = start;
+    ult->stack = stack;
+    ult->args = args;
+
+    // Initialize the context
+    memset(&(ult->context), 0, sizeof(struct context));
+
+    printf("[*] ultcreate(tid: %d, ra: %p, sp: %p)\n", ult->ultid, start, stack);
+
+    return true;
 }
 
 /* Thread scheduler */
-void ulthread_schedule(void) {
-    
-    /* Add this statement to denote which thread-id is being scheduled next */
-    printf("[*] ultschedule (next tid: %d)\n", 0);
+void ulthread_schedule(void)
+{
+    ulthread *prev_thread = current_thread;
+    ulthread *next_thread = NULL;
 
+    if (scheduling_algorithm == ROUNDROBIN)
+    {
+        // Round-robin scheduling
+        int next_tid = (current_thread->ultid + 1) % MAXULTHREADS;
+        for (int i = 0; i < MAXULTHREADS; i++)
+        {
+            int index = (next_tid + i) % MAXULTHREADS;
+            if (ulthred_table[index].state == RUNNABLE)
+            {
+                next_thread = &ulthred_table[index];
+                break;
+            }
+            else if (ulthred_table[index].state == YIELD)
+            {
+                // Skip YIELD threads this time; set them back to RUNNABLE
+                ulthred_table[index].state = RUNNABLE;
+            }
+        }
+    }
+    else if (scheduling_algorithm == PRIORITY)
+    {
+        // Priority scheduling
+        uint64 highest_priority = 0xFFFFFFFFFFFFFFFF;
+        for (int i = 0; i < MAXULTHREADS; i++)
+        {
+            if (ulthred_table[i].state == RUNNABLE)
+            {
+                if (ulthred_table[i].priority < highest_priority)
+                {
+                    highest_priority = ulthred_table[i].priority;
+                    next_thread = &ulthred_table[i];
+                }
+            }
+            else if (ulthred_table[i].state == YIELD)
+            {
+                // Skip YIELD threads this time; set them back to RUNNABLE
+                ulthred_table[i].state = RUNNABLE;
+            }
+        }
+    }
+    else if (scheduling_algorithm == FCFS)
+    {
+        // First-Come-First-Serve scheduling
+        uint64 earliest_time = 0xFFFFFFFFFFFFFFFF;
+        for (int i = 0; i < MAXULTHREADS; i++)
+        {
+            if (ulthred_table[i].state == RUNNABLE)
+            {
+                if (ulthred_table[i].creation_time < earliest_time)
+                {
+                    earliest_time = ulthred_table[i].creation_time;
+                    next_thread = &ulthred_table[i];
+                }
+            }
+            else if (ulthred_table[i].state == YIELD)
+            {
+                // Skip YIELD threads this time; set them back to RUNNABLE
+                ulthred_table[i].state = RUNNABLE;
+            }
+        }
+    }
+
+    /* Add this statement to denote which thread-id is being scheduled next */
+    printf("[*] ultschedule (next tid: %d)\n", next_thread->ultid);
+    current_thread = next_thread;
     // Switch between thread contexts
-    ulthread_context_switch(NULL, NULL);
+    ulthread_context_switch(&prev_thread->context, &next_thread->context);
 }
 
 /* Yield CPU time to some other thread. */
-void ulthread_yield(void) {
-
-    /* Please add thread-id instead of '0' here. */
-    printf("[*] ultyield(tid: %d)\n", 0);
+void ulthread_yield(void)
+{
+    current_thread->state = YIELD;
+    printf("[*] ultyield(tid: %d)\n", current_thread->ultid);
+    ulthread_schedule();
 }
 
 /* Destroy thread */
--- xv6-riscv/user/ulthread.h
+++ xv6-riscv/user/ulthread.h
@@ -5,16 +5,48 @@
 
 #define MAXULTHREADS 100
 
-enum ulthread_state {
+typedef enum ulthread_state
+{
   FREE,
   RUNNABLE,
   YIELD,
-};
+} ulthread_state;
 
-enum ulthread_scheduling_algorithm {
-  ROUNDROBIN,   
-  PRIORITY,     
-  FCFS,         // first-come-first serve
-};
+typedef enum ulthread_scheduling_algorithm
+{
+  ROUNDROBIN,
+  PRIORITY,
+  FCFS, // first-come-first serve
+} ulthread_scheduling_algorithm;
+
+typedef struct context
+{
+  uint64 ra;
+  uint64 sp;
+  uint64 s0;
+  uint64 s1;
+  uint64 s2;
+  uint64 s3;
+  uint64 s4;
+  uint64 s5;
+  uint64 s6;
+  uint64 s7;
+  uint64 s8;
+  uint64 s9;
+  uint64 s10;
+  uint64 s11;
+} context;
+
+typedef struct ulthread
+{
+  uint64 ultid;
+  uint64 start; // function starting address
+  uint64 stack; // stack address
+  uint64 priority;
+  ulthread_state state;
+  uint64 *args;
+  struct context *context; // context save space
+  uint64 creation_time;    // Time when thread was created (for FCFS)
+} ulthread;
 
 #endif
